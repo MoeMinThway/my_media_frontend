ffbbeeeecf8aee29ae0034636a7bbc72ed9aa30f
MoeMinThway <moeminthway.mink@gmail.com>
2024-04-11 20:33:46 +0630

    4/11/2024

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -6,23 +6,19 @@
     </div>
   </div>
    <!-- <br> -->
-  <div class="container col-6 shadow offset-3 my-5 p-5" style="height: 400px;">
+  <div class="container col-4 shadow offset-4 my-5 p-5" style="height: 400px;">
   
     <form>
         <div class="form-group">
             <label for="exampleInputEmail1">Email address</label>
             <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
         </div>
         <div class="form-group">
             <label for="exampleInputPassword1">Password</label>
             <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
         </div>
-        <div class="form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+    
+        <button type="submit" class="btn btn-primary">Login</button>
     </form>
 
   </div>
